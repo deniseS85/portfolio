2cf1359bcfd7f4c73e0ae0938fa1840daa479e1f
deniseS85 <denisabel78@hotmail.de>
2023-04-22 11:57:13 +0200

    portfolio

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,20 @@ import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { AboutMeComponent } from './about-me/about-me.component';
 import { MySkillsComponent } from './my-skills/my-skills.component';
+import { PortfolioComponent } from './portfolio/portfolio.component';
+import { ContactComponent } from './contact/contact.component';
+import { FooterComponent } from './footer/footer.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     AboutMeComponent,
-    MySkillsComponent
+    MySkillsComponent,
+    PortfolioComponent,
+    ContactComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
--- src/app/my-skills/my-skills.component.ts
+++ src/app/my-skills/my-skills.component.ts
@@ -6,5 +6,10 @@ import { Component } from '@angular/core';
   styleUrls: ['./my-skills.component.scss']
 })
 export class MySkillsComponent {
+  skillsNames = ['Angular', 'TypeScript', 'JavaScript', 'HTML', 'Firebase', 'GIT', 
+                'CSS', 'Rest-Api', 'Scrum', 'Material Design'];
+  skillsIcons = ['angular-icon.png', 'typescript-icon.png', 'javascript-icon.png',
+                'html-icon.png', 'firebase-icon.png', 'git-icon.png', 'css-icon.png',
+                'rest-api-icon.png', 'scrum-icon.png', 'material-design-icon.png'];
 
 }
