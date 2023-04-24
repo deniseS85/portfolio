89af3f2976b781c98b4d758e4f8b57b898b05c3c
deniseS85 <denisabel78@hotmail.de>
2023-04-24 09:39:25 +0200

    finished nav, highlights, scroll

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AboutMeComponent } from './about-me/about-me.component';
+import { MySkillsComponent } from './my-skills/my-skills.component';
+import { PortfolioComponent } from './portfolio/portfolio.component';
+import { HeaderComponent } from './header/header.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: HeaderComponent},
+  { path: 'aboutMe', component: AboutMeComponent },
+  { path: 'skills', component: MySkillsComponent },
+  { path: 'portfolio', component: PortfolioComponent },
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes, { anchorScrolling: 'enabled'})],
-  exports: [RouterModule]
+  imports: [RouterModule.forRoot(routes, { anchorScrolling: 'enabled', scrollOffset: [0, 160] })],
+  exports: [RouterModule],
 })
 export class AppRoutingModule { }
--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -6,10 +6,10 @@ import { ViewportScroller } from '@angular/common';
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss']
 })
+
 export class HeaderComponent {
   constructor(private viewportScroller: ViewportScroller) {}
-  status: boolean = false;
-
+  
   public scroll(elementId: string): void { 
     this.viewportScroller.scrollToAnchor(elementId);
   }
