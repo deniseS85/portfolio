3524c7201faf421f1d608ca95eeb4c51b39e2819
deniseS85 <denisabel78@hotmail.de>
2023-04-25 10:47:27 +0200

    highlight links by scrolling

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
 import { ViewportScroller } from '@angular/common';
 
 @Component({
@@ -9,10 +9,28 @@ import { ViewportScroller } from '@angular/common';
 
 export class HeaderComponent {
   constructor(private viewportScroller: ViewportScroller) {}
-  
+
+  currentActive: number = 0;
+
   public scroll(elementId: string): void { 
     this.viewportScroller.scrollToAnchor(elementId);
   }
+
+
+@HostListener('window:scroll', ['$event'])
+  checkOffsetTop() {
+    console.log(window.pageYOffset); 
+    if (window.pageYOffset > 734 && window.pageYOffset < 1297) {
+      this.currentActive = 1;
+    } else if (window.pageYOffset > 1298 && window.pageYOffset < 1851) {
+      this.currentActive = 2;
+    } else if (window.pageYOffset > 1852 && window.pageYOffset < 4307) {
+      this.currentActive = 3;
+    } else {
+      this.currentActive = 0;
+    }
+  }
 }
+
 
 
--- src/app/portfolio/portfolio.component.ts
+++ src/app/portfolio/portfolio.component.ts
@@ -8,11 +8,48 @@ import { Component } from '@angular/core';
 export class PortfolioComponent {
 
   projects = [
-    { image: '', title: '', languages: '', description: '' },
-    { image: '', title: '', languages: '', description: '' },
-    { image: '', title: '', languages: '', description: '' },
-    { image: '', title: '', languages: '', description: '' },
-    { image: '', title: '', languages: '', description: '' },
+      { 
+        image: 'assets/img/join-photo.png', 
+        title: 'Join', 
+        languages: 'JavaScript | HTML | CSS | GIT', 
+        description: 'Task manager inspired by the Kanban System. Create and organize tasks using drag and drop functions, assign users and categories.',
+        linkLiveTest: '',
+        linkGitHub: 'https://github.com/deniseS85/Join-Kanban' 
+      },
+      { 
+        image: 'assets/img/pollo_loco-photo.png', 
+        title: 'El Pollo Loco', 
+        languages: 'JavaScript | HTML | CSS', 
+        description: 'A simple Jump-and-Run game based on an object-oriented approach. Help Pepe to find coins and bottles to fight against the chicken boss.',
+        linkLiveTest: '',
+        linkGitHub: 'https://github.com/deniseS85/El-Pollo-Loco' 
+      },
+      { 
+        image: 'assets/img/pokedex-photo.png', 
+        title: 'Pokédex', 
+        languages: 'JavaScript | HTML | CSS | Api', 
+        description: 'Based on the PokéAPI a simple library that provides and catalogues pokemon information.', 
+        linkLiveTest: '',
+        linkGitHub: 'https://github.com/deniseS85/Pokedex' 
+      },
+      { 
+        image: 'assets/img/crm-photo.png', 
+        title: 'Simple CRM', 
+        languages: 'Angular | Firebase', 
+        description: 'A very Simple Customer Relationship Management system working with CRUD functionality.',
+        linkLiveTest: '',
+        linkGitHub: 'https://github.com/deniseS85/Pokedex'
+    },
+      { 
+        image: 'assets/img/join-photo.png', 
+        title: 'Shopping List', 
+        languages: 'JavaScript | HTML | CSS', 
+        description: 'A personalised shopping list that allows you to quickly and easily select, modify and move products to the shopping basket by clicking on the product image. These can be sent directly via Whatsapp.', 
+        linkLiveTest: '',
+        linkGitHub: 'https://github.com/deniseS85/Einkaufsliste'
+      },
   ];
-
 }
+
+
+
