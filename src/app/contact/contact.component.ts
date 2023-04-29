57fa0999698fe78efa3ae5be2d516063d2b25b94
deniseS85 <denisabel78@hotmail.de>
2023-04-29 12:33:53 +0200

    formbuilder

--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -1,41 +1,31 @@
 import { ViewportScroller } from '@angular/common';
 import { Component, ElementRef, ViewChild, Input } from '@angular/core';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { FormBuilder, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-contact',
   templateUrl: './contact.component.html',
   styleUrls: ['./contact.component.scss']
 })
+
+
 export class ContactComponent {
-  constructor(private viewportScroller: ViewportScroller) {}
-  
-  @Input() nameInvalid = false;
-  @Input() emailInvalid = false;
-  @Input() messageInvalid = false;
-  @ViewChild('form') form!: ElementRef;
-  @ViewChild('nameField') nameField!: ElementRef;
-  @ViewChild('mailField') mailField!: ElementRef;
-  @ViewChild('messageField') messageField!: ElementRef;
-  @ViewChild('sendBtn') sendBtn!: ElementRef;
+  minLength: number = 20;
+  isSubmitted: boolean = false;
+  emailSent: boolean = false;
+  emailNotSent: boolean = false;
 
-  public scroll(elementId: string): void { 
-    this.viewportScroller.scrollToAnchor(elementId);
-  }
+  contactForm = this.fb.group({
+    name: ['', Validators.required],
+    email: ['', [Validators.required, Validators.email]],
+    message: ['', [Validators.required, Validators.minLength(this.minLength)]],
+});
+
+  constructor(private viewportScroller: ViewportScroller, private fb: FormBuilder) {}
+    
 
 
-  nameIsInvalid(){
-    this.nameInvalid=true;
-  }
-
-  emailIsInvalid(){
-    this.emailInvalid=true;
-  }
-
-  messageIsinvalid(){
-    this.messageInvalid=true;
-  }
-
+/* 
   async sendMail() {
     let nameField = this.nameField.nativeElement;
     let mailField = this.mailField.nativeElement;
@@ -46,10 +36,10 @@ export class ContactComponent {
     mailField.disabled = true;
     messageField.disabled = true;
     sendBtn.disabled = true;
-
+ */
     //animation senden
 
-    let fd = new FormData();
+    /* let fd = new FormData();
     fd.append('name', nameField.value);
     fd.append('email', mailField.value);
     fd.append('message', messageField.value);
@@ -61,16 +51,56 @@ export class ContactComponent {
         body: fd
       }
     );
-    
+     */
 
     // text anzeigen gesendet
-    nameField.disabled = false;
+   /*  nameField.disabled = false;
     mailField.disabled = false;
     messageField.disabled = false;
     nameField.value = '';
     mailField.value = '';
     messageField.value = '';
+  } */
+
+
+  sendMail() {
+    const data:any = this.contactForm.value;
+    const fd = new FormData();
+    fd.append('name', data.name);
+    fd.append('email', data.email);
+    fd.append('message', data.message);
+    this.isSubmitted = true;
+    /* this.contactForm.reset(); */
+    console.log(this.contactForm.value, this.contactForm.invalid)
+
+    /* await fetch('https://denise-siegl.developerakademie.net/send_mail/send_mail.php',
+    {
+      method: 'POST',
+      body: this.setFormData()
+    }
+  );
+
+    this.isSubmitted = true;
+
+    setTimeout(() => {
+        this.isSubmitted = false;
+    }, 2000);
+
+    this.contactForm.reset(); */
   }
 
+  /* setFormData() {
+    const data:any = this.contactForm.value;
+    const fd = new FormData();
+    fd.append('name', data.name);
+    fd.append('email', data.email);
+    fd.append('message', data.message);
+    return fd;
+  } */
+
+
+  public scroll(elementId: string): void { 
+    this.viewportScroller.scrollToAnchor(elementId);
+  }
 }
 
